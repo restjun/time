e0cd9994b8e1b77d7f0bb75a8a010c58a068b3f0
restjun <150533379+restjun@users.noreply.github.com>
2025-05-28 09:45:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def find_golden_cross_coins(tickers, interval, count):
         if df is not None and len(df) >= 2:
             vwma_1 = calculate_vwma(df['close'].values, df['volume'].values, 20)
             vwma_2 = calculate_vwma(df['close'].values, df['volume'].values, 50)
-            if vwma_1 is not None and vwma_2 is not None and vwma_1 < vwma_2:
+            if vwma_1 is not None and vwma_2 is not None and vwma_1 > vwma_2:
                 golden_cross_coins.append(ticker)
 
     return golden_cross_coins
@@ -215,7 +215,7 @@ def send_golden_death_cross_message(golden_cross_coins, death_cross_coins, btc_s
         if price_change_percentage is not None and price_change_percentage > -10:
             is_in_golden_list = coin in golden_trade_price_result
             is_new_coin = coin in new_death_coins
-            message_lines.append(f"{idx}.{'🟩' if is_new_coin else ''} {'〽️' if is_in_golden_list else ''} {coin.replace('KRW-', '')}: {trade_price}억 ({price_change_percentage:+.2f}%) {'🚀' if is_new_coin else ''}")
+            message_lines.append(f"{idx}.{'🟩' if is_new_coin else ''} {'〽' if is_in_golden_list else '〽️'} {coin.replace('KRW-', '')}: {trade_price}억 ({price_change_percentage:+.2f}%) {'🚀' if is_new_coin else ''}")
             previous_trade_prices[coin] = trade_price
             
     message = "\n".join(message_lines)
