715efdeb0fbdd15de2c6bc8e82c537768125cb64
restjun <150533379+restjun@users.noreply.github.com>
2025-06-01 08:51:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -95,8 +95,8 @@ def find_golden_cross_coins(tickers, interval, count):
     for ticker in tickers:
         df = retry_request(pyupbit.get_ohlcv, ticker, interval=interval, count=count)
         if df is not None and len(df) >= 2:
-            vwma_1 = calculate_vwma(df['close'].values, df['volume'].values, 5)
-            vwma_2 = calculate_vwma(df['close'].values, df['volume'].values, 20)
+            vwma_1 = calculate_vwma(df['close'].values, df['volume'].values, 20)
+            vwma_2 = calculate_vwma(df['close'].values, df['volume'].values, 50)
             if vwma_1 is not None and vwma_2 is not None and vwma_1 < vwma_2:
                 golden_cross_coins.append(ticker)
 
@@ -194,7 +194,7 @@ def send_golden_death_cross_message(golden_cross_coins, death_cross_coins, btc_s
     message_lines.append("----------------------------------")
     message_lines.append("🌟 배은산 박현준 박현서 우리 가족 사랑해 🌟")
     message_lines.append("----------------------------------")
-    message_lines.append("🟥 5 < 20  ")
+    message_lines.append("🟥 20 < 50  ")
     message_lines.append("----------------------------------")
    
     for idx, (coin, trade_price) in enumerate(sorted(golden_trade_price_result.items(), key=lambda x: x[1], reverse=True)[:15], start=1):
@@ -204,7 +204,7 @@ def send_golden_death_cross_message(golden_cross_coins, death_cross_coins, btc_s
             message_lines.append(f"{idx}.{coin.replace('KRW-', '')}: {trade_price}억 ({price_change_percentage:+.2f}%) {'🚀' if is_new_coin else ''}")
     
     message_lines.append("----------------------------------")
-    message_lines.append("🟥 50 < 200 역배열 눌림돌파 ")
+    message_lines.append("🟥 50 < 200 역배열 하락추세 ")
     message_lines.append("----------------------------------")
     
     for idx, (coin, trade_price) in enumerate(sorted(death_trade_price_result.items(), key=lambda x: x[1], reverse=True)[:10], start=1):
