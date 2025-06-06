fc7b17aa17b1986bcf987693dceff19bc51e6bc7
restjun <150533379+restjun@users.noreply.github.com>
2025-06-06 13:27:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def send_telegram_message(message, btc_status_1h, btc_status_4h, is_new_coin=Fal
     for retry_count in range(1, max_retries + 1):
         try:
             # 메시지와 BTC 상태를 함께 보내기
-            message_with_status = f"{message}\n\n(BTC-시간){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_1h else '  🟥🟥🟥🟥🟥 (추세하락)'}\n(BTC-일봉){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_4h else '🟥🟥🟥🟥🟥 (추세하락)'}"
+            message_with_status = f"{message}\n\n(BTC-시간){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_1h else ' 🟥🟥🟥🟥🟥 (추세하락)'}\n(BTC-일봉){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_4h else '🟥🟥🟥🟥🟥 (추세하락)'}"
             if is_new_coin:
                 message_with_status += ""
             bot.sendMessage(chat_id=telegram_user_id, text=message_with_status)
