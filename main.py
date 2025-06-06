2600909422cb4316ecff0244fa0cc3fc7b9c1124
restjun <150533379+restjun@users.noreply.github.com>
2025-06-06 13:53:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def send_telegram_message(message, btc_status_1h, btc_status_4h, is_new_coin=Fal
     for retry_count in range(1, max_retries + 1):
         try:
             # 메시지와 BTC 상태를 함께 보내기
-            message_with_status = f"{message}\n\n(BTC-1시간-50>200){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_1h else ' 🟥🟥🟥🟥🟥 (추세하락)'}\n(BTC-일봉-1>2){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_4h else '🟥🟥🟥🟥🟥 (추세하락)'}"
+            message_with_status = f"{message}\n\n(BTC-일봉-1>2){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_1h else ' 🟥🟥🟥🟥🟥 (추세하락)'}\n(BTC-1시간-50>200){' 🟩🟩🟩🟩🟩 (추세상승)' if btc_status_4h else '🟥🟥🟥🟥🟥 (추세하락)'}"
             if is_new_coin:
                 message_with_status += ""
             bot.sendMessage(chat_id=telegram_user_id, text=message_with_status)
@@ -68,16 +68,16 @@ def calculate_vwma(data, volume, period):
 # 비트코인 상태 확인 함수 업데이트
 def check_bitcoin_status():
     btc_ticker = "KRW-BTC"
-    btc_df = retry_request(pyupbit.get_ohlcv, btc_ticker, interval="minute60", count=200)
+    btc_df = retry_request(pyupbit.get_ohlcv, btc_ticker, interval="minute1440", count=200)
     if btc_df is not None and len(btc_df) >= 200:
-        btc_vwma_1 = calculate_vwma(btc_df['close'].values, btc_df['volume'].values, 50)
-        btc_vwma_2 = calculate_vwma(btc_df['close'].values, btc_df['volume'].values, 200)
+        btc_vwma_1 = calculate_vwma(btc_df['close'].values, btc_df['volume'].values, 1)
+        btc_vwma_2 = calculate_vwma(btc_df['close'].values, btc_df['volume'].values, 2)
         btc_status_1h = 1 if btc_vwma_1 is not None and btc_vwma_2 is not None and btc_vwma_1 > btc_vwma_2 else 0
 
-        btc_df_4h = retry_request(pyupbit.get_ohlcv, btc_ticker, interval="minute1440", count=200)
+        btc_df_4h = retry_request(pyupbit.get_ohlcv, btc_ticker, interval="minute60", count=200)
         if btc_df_4h is not None and len(btc_df_4h) >= 200:
-            btc_vwma_1_4h = calculate_vwma(btc_df_4h['close'].values, btc_df_4h['volume'].values, 1)
-            btc_vwma_2_4h = calculate_vwma(btc_df_4h['close'].values, btc_df_4h['volume'].values, 2)
+            btc_vwma_1_4h = calculate_vwma(btc_df_4h['close'].values, btc_df_4h['volume'].values, 50)
+            btc_vwma_2_4h = calculate_vwma(btc_df_4h['close'].values, btc_df_4h['volume'].values, 200)
             btc_status_4h = 1 if btc_vwma_1_4h is not None and btc_vwma_2_4h is not None and btc_vwma_1_4h > btc_vwma_2_4h else 0
         else:
             logging.error("비트코인 4시간 데이터를 불러올 수 없습니다.")
