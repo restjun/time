5b9509a92dba5cb5211c8cb354cc182370c6c925
restjun <150533379+restjun@users.noreply.github.com>
2025-06-06 09:06:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ def send_golden_death_cross_message(golden_cross_coins, death_cross_coins, btc_s
         if price_change_percentage is not None and price_change_percentage > -10:
             is_in_golden_list = coin in golden_trade_price_result
             is_new_coin = coin in new_death_coins
-            message_lines.append(f"{idx}.{'🟩' if is_new_coin else '🟩'} {'🅾️' if is_in_golden_list else ''} {coin.replace('KRW-', '')}: {trade_price}억 ({price_change_percentage:+.2f}%) {'🚀' if is_new_coin else ''}")
+            message_lines.append(f"{idx}.{'🟩' if is_new_coin else '🟩'} {'✅️' if is_in_golden_list else ''} {coin.replace('KRW-', '')}: {trade_price}억 ({price_change_percentage:+.2f}%) {'🚀' if is_new_coin else ''}")
             previous_trade_prices[coin] = trade_price
             
     message = "\n".join(message_lines)
