61a0391e34b886814c244687b29d45c7b255da41
restjun <150533379+restjun@users.noreply.github.com>
2025-06-07 09:22:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -197,8 +197,8 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
         vwma_200 = calculate_vwma(df['close'].values, df['volume'].values, 200) if df is not None else None
 
         five_twenty = "(🟥)" if vwma_5 is not None and vwma_20 is not None and vwma_5 < vwma_20 else "🟩"
-        twenty_fifty = "(🟥)" if vwma_20 is not None and vwma_50 is not None and vwma_20 < vwma_50 else "🟩"
-        fifty_two_hundred = "(🟥)" if vwma_50 is not None and vwma_200 is not None and vwma_50 < vwma_200 else "🟩"
+        twenty_fifty = "🟥" if vwma_20 is not None and vwma_50 is not None and vwma_20 < vwma_50 else "🟩"
+        fifty_two_hundred = "🟥" if vwma_50 is not None and vwma_200 is not None and vwma_50 < vwma_200 else "🟩"
 
         # 줄바꿈 추가 및 랭크 번호 포함
         message_lines.append(
