52bf4e5400593d3e5a2edb9d0505177f4995b2eb
restjun <150533379+restjun@users.noreply.github.com>
2025-06-12 00:46:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def find_golden_cross_coins(tickers, interval, count):
     for ticker in tickers:
         df = retry_request(pyupbit.get_ohlcv, ticker, interval=interval, count=count)
         if df is not None and len(df) >= 2:
-            vwma_1 = calculate_vwma(df['close'].values, df['volume'].values, 200)
+            vwma_1 = calculate_vwma(df['close'].values, df['volume'].values, 20)
             vwma_2 = calculate_vwma(df['close'].values, df['volume'].values, 50)
             if vwma_1 is not None and vwma_2 is not None and vwma_1 > vwma_2:
                 golden_cross_coins.append(ticker)
@@ -179,7 +179,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
 
     message_lines = []
     message_lines.append("----------------------------------")
-    message_lines.append("[15분] 50-200 역배열 / 🅾️ ↗️ 🟩 돌파매수")
+    message_lines.append("[15분] 20-50 정배열 / 🅾️ ↗️ 🟩 돌파매수")
     message_lines.append("----------------------------------")
     message_lines.append("(원칙) 1️⃣:🅾️➖️✅️➖️🟥  ▶️  🟩➖️🟩➖️🟥 ")
     message_lines.append("----------------------------------")
