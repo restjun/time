2718410b020ae3c4c20e9e5a4788eddbbac72f65
restjun <150533379+restjun@users.noreply.github.com>
2025-06-12 19:01:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
     message_lines.append("----------------------------------")
     message_lines.append("[🟩] 일봉 / 🟥 ↗️ 🟩 돌파매수")
     message_lines.append("----------------------------------")
-    message_lines.append("(원칙) 1️⃣:🅾️➖️✅️➖️🅾️  ▶️  🟩➖️🟩➖️🟥 "
+    message_lines.append("(원칙) 1️⃣:🅾️➖️✅️➖️🅾️  ▶️  🟩➖️🟩➖️🟥 ")
     message_lines.append("(원칙) 2️⃣:✅️➖️🅾️➖️✅️  ▶️  🟩➖️🟩➖️🟩 ")
                          
     
