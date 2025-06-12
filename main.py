1005fd229dffc3000d3ba0f5343bcf4b92bca706
restjun <150533379+restjun@users.noreply.github.com>
2025-06-12 23:11:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
     message_lines.append("----------------------------------")
     message_lines.append("[🟩] 일봉 / 🟥 ↗️ 🟩 돌파매수")
     message_lines.append("----------------------------------")
-    message_lines.append("(원칙) 1️⃣:🅾️➖️🟩➖️🟩  ▶️  🟩➖️🟩➖️🟩")
+    message_lines.append("(원칙) 1️⃣:🅾️➖️🟩➖️🟥  ▶️  🟩➖️🟩➖️🟩")
     message_lines.append("----------------------------------")
 
     for idx, (coin, trade_price) in enumerate(sorted(golden_trade_price_result.items(), key=lambda x: x[1], reverse=True), start=1):
@@ -191,15 +191,15 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
         # VWMA 상태 계산
         df = retry_request(pyupbit.get_ohlcv, coin, interval="minute60", count=200)
         vwma_5 = calculate_vwma(df['close'].values, df['volume'].values, 5) if df is not None else None
-        vwma_10 = calculate_vwma(df['close'].values, df['volume'].values, 10) if df is not None else None
+        vwma_10 = calculate_vwma(df['close'].values, df['volume'].values, 20) if df is not None else None
         vwma_20 = calculate_vwma(df['close'].values, df['volume'].values, 20) if df is not None else None
         vwma_50 = calculate_vwma(df['close'].values, df['volume'].values, 50) if df is not None else None
         vwma_200 = calculate_vwma(df['close'].values, df['volume'].values, 200) if df is not None else None
 
 
-        five_twenty = " 🟩" if vwma_5 is not None and vwma_10 is not None and vwma_5 > vwma_10 else " 🅾️"
-        twenty_fifty = "🟩" if vwma_10 is not None and vwma_20 is not None and vwma_10 > vwma_20 else "🟥"
-        fifty_two_hundred = "🟩" if vwma_20 is not None and vwma_50 is not None and vwma_20 > vwma_50 else "🟥"
+        five_twenty = " 🟩" if vwma_5 is not None and vwma_20 is not None and vwma_5 > vwma_10 else " 🅾️"
+        twenty_fifty = "🟩" if vwma_20 is not None and vwma_50 is not None and vwma_20 > vwma_50 else "🟥"
+        fifty_two_hundred = "🟩" if vwma_50 is not None and vwma_200 is not None and vwma_50 > vwma_200 else "🟥"
 
         # 줄바꿈 추가 및 랭크 번호 포함
         message_lines.append(
