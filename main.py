d7dd9fc1822416a6fcf280241b9a9c736af31936
restjun <150533379+restjun@users.noreply.github.com>
2025-06-12 15:25:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
     message_lines.append("----------------------------------")
     message_lines.append("[🟩] 일봉 / 🟥 ↗️ 🟩 돌파매수")
     message_lines.append("----------------------------------")
-    message_lines.append("(원칙) 1️⃣:🟥➖️✅️➖️🅾️  ▶️  🟩➖️🟩➖️🟥 ")
+    message_lines.append("(원칙) 1️⃣:🅾️➖️✅️➖️🅾️  ▶️  🟩➖️🟩➖️🟥 ")
     message_lines.append("----------------------------------")
 
     for idx, (coin, trade_price) in enumerate(sorted(golden_trade_price_result.items(), key=lambda x: x[1], reverse=True), start=1):
@@ -197,7 +197,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
         vwma_200 = calculate_vwma(df['close'].values, df['volume'].values, 200) if df is not None else None
 
 
-        five_twenty = " 🟩" if vwma_5 is not None and vwma_20 is not None and vwma_5 > vwma_20 else " 🟥"
+        five_twenty = " 🟩" if vwma_5 is not None and vwma_20 is not None and vwma_5 > vwma_20 else " 🅾️"
         twenty_fifty = "✅️" if vwma_20 is not None and vwma_50 is not None and vwma_20 > vwma_50 else "🟥"
         fifty_two_hundred = "🟩" if vwma_100 is not None and vwma_200 is not None and vwma_100 > vwma_200 else "🅾️"
 
