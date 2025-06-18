063fad550e538c0f0f304bcf7a73dc35c7b516ad
restjun <150533379+restjun@users.noreply.github.com>
2025-06-18 17:01:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
         vwma_120 = calculate_vwma(df['close'].values, df['volume'].values, 120) if df is not None else None
 
 
-        five_twenty = " ✅️" if vwma_5 is not None and vwma_20 is not None and vwma_5 > vwma_20 else " 🅾️"
+        five_twenty = " ⬛" if vwma_5 is not None and vwma_20 is not None and vwma_5 > vwma_20 else " 🅾️"
         twenty_fifty = "✅️" if vwma_20 is not None and vwma_60 is not None and vwma_20 > vwma_60 else "🅾️"
         fifty_two_hundred = "✅️" if vwma_60 is not None and vwma_120 is not None and vwma_60 > vwma_120 else "🅾️"
 
