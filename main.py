87753b90f5e0e345bc95e5920fcafc374444380c
restjun <150533379+restjun@users.noreply.github.com>
2025-06-20 12:37:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
             f"{idx}.{five_twenty}-{twenty_fifty}-{fifty_two_hundred}  {coin.replace('KRW-', '')} : {trade_price}억 ({price_change_str}) ")
 
     message_lines.append("----------------------------------")
-    message_lines.append("(알트-[집중]) 🟩 [ ❓️ ] 🅾️-✅️-✅️")
+    message_lines.append("(알트-[집중]) 🟩 [ ❓️ ] 🅾️-✅️-🅾️")
     
     final_message = "\n".join(message_lines)
     send_telegram_message(final_message, btc_status_1h, btc_status_4h)
