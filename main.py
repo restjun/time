ab8ba6eb8e0113450069f055ab51c6e142d545fb
restjun <150533379+restjun@users.noreply.github.com>
2025-06-28 12:08:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
         return
 
     message_lines = []
-    message_lines.append("3️⃣/5️⃣ 자리(🎯 직전고점 돌파 2️⃣0️⃣➖️5️⃣0️⃣)")
+    message_lines.append("3️⃣/5️⃣자리(🎯1차 정배열 +1~3시간 2차 정배열 지점만 매수)")
     message_lines.append("----------------------------------")
 
     idx = 1
