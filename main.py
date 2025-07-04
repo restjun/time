197632cb92ad0db1880a36e7ece0b9715347c425
restjun <150533379+restjun@users.noreply.github.com>
2025-07-05 06:21:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -95,8 +95,8 @@ def find_golden_cross_coins(tickers, interval, count):
     for ticker in tickers:
         df = retry_request(pyupbit.get_ohlcv, ticker, interval=interval, count=count)
         if df is not None and len(df) >= 2:
-            vwma_1 = calculate_vwma(df['close'].values, df['volume'].values, 50)
-            vwma_2 = calculate_vwma(df['close'].values, df['volume'].values, 20)
+            vwma_1 = calculate_vwma(df['close'].values, df['volume'].values, 20)
+            vwma_2 = calculate_vwma(df['close'].values, df['volume'].values, 5)
             if vwma_1 is not None and vwma_2 is not None and vwma_1 > vwma_2:
                 golden_cross_coins.append(ticker)
 
@@ -177,7 +177,7 @@ def send_golden_cross_message(golden_cross_coins, btc_status_1h, btc_status_4h, 
         return
 
     message_lines = []
-    message_lines.append("🎯 50-20 출발자리만 하자")
+    message_lines.append("🎯 20-5 출발자리만 하자")
     message_lines.append("----------------------------------")
 
     idx = 1
